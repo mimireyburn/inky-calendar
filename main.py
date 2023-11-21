c16b2a600770d69b10b1e4b969f6b72ea0ca493d
Mimi Reyburn <mimireyburn@gmail.com>
2023-11-21 16:46:09 +0000

    bug fix

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from PIL import Image
 import time
 from inky import InkyPHAT
