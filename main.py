9155450e38702be1e541a5a75b8575236f0c59f4
Mimi Reyburn <mimireyburn@gmail.com>
2023-11-21 18:01:48 +0000

    Add inky script

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import subprocess
+from inky.auto import auto
 
 # Path to your local HTML file
 html_file_path = '/home/calendar/inky-calendar/index.html'
@@ -11,3 +12,15 @@ command = f'wkhtmltoimage --quality 100 --javascript-delay 100000 {html_file_pat
 
 # Execute the command
 subprocess.run(command, shell=True)
+
+
+print("Displaying calendar")
+saturation = 1.0
+
+# display calendar_image.png on the screen
+inky_display = auto(ask_user=True, verbose=True)
+# inky_display.set_border(inky_display.WHITE)s
+image = Image.open("calendar.png")
+inky_display.set_image(image, saturation=saturation)
+inky_display.set_border(inky_display.WHITE)
+inky_display.show()
