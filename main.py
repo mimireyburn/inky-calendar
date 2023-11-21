dbefa65203bea05694c795551001099bd0149d60
Mimi Reyburn <mimireyburn@gmail.com>
2023-11-21 17:52:42 +0000

    add delay

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ html_file_path = '/home/calendar/inky-calendar/index.html'
 screenshot_path = '/home/calendar/inky-calendar/calendar.png'
 
 # Command to render HTML to an image
-command = f'wkhtmltoimage --quality 100 {html_file_path} {screenshot_path}'
+command = f'wkhtmltoimage --quality 100 --javascript-delay 100000 {html_file_path} {screenshot_path}'
 
 # Execute the command
 subprocess.run(command, shell=True)
