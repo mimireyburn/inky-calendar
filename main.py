14e4a0c37aa2bd5ce36b6b9cb63f83259a9baff7
Mimi Reyburn <mimireyburn@gmail.com>
2023-11-21 18:36:10 +0000

    bug fix image size

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ html_file_path = '/home/calendar/inky-calendar/index.html'
 screenshot_path = '/home/calendar/inky-calendar/calendar.png'
 
 # Command to render HTML to an image
-command = f'wkhtmltoimage --quality 100 {html_file_path} {screenshot_path}'
+command = f'wkhtmltoimage --quality 100 --javascript-delay 100000 --width 800 --height 480 {html_file_path} {screenshot_path}'
 
 # Execute the command
 subprocess.run(command, shell=True)
