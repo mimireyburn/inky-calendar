0a22c534d1b29a1c6f7d4e4c2d5f21400738127d
Mimi Reyburn <mimireyburn@gmail.com>
2023-11-28 14:51:10 +0000

    Debug UI

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class InkyCalendar:
         self.inky_display = auto(ask_user=True, verbose=True)
 
     def render_html_to_image(self):
-        command = f'wkhtmltoimage --quality 100 --javascript-delay 25000 --width 800 --height 480 {self.html_file_path} {self.screenshot_path}'
+        command = f'wkhtmltoimage --quality 100 --javascript-delay 25000 --width 810 --height 530 {self.html_file_path} {self.screenshot_path}'
         subprocess.run(command, shell=True)
 
     def display_calendar(self):
@@ -22,9 +22,11 @@ class InkyCalendar:
         enhancer = ImageEnhance.Color(image)
         image = enhancer.enhance(self.saturation)
 
+        print("Display dimenstions", self.inky_display.resolution, "WH:", self.inky_display.WIDTH, self.inky_display.HEIGHT)
+
         # Crop image to fit screen
         buffer = 10
-        image = image.crop((buffer, buffer, self.inky_display.WIDTH + buffer, self.inky_display.HEIGHT + buffer))
+        image = image.crop((buffer, buffer, self.inky_display.WIDTH + buffer, self.inky_display.HEIGHT+ buffer))
 
         # Save image for debugging
         image.save(self.screenshot_path)
